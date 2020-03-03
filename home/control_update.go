493175e04d4d579b56635d738b4011f9ac2eeb61
Simon Zolin <s.zolin@adguard.com>
2020-03-03 19:22:03 +0300

    * auto-upgrade: don't show Upgrade button on UNIX if running under non-root user

--- home/control_update.go
+++ home/control_update.go
@@ -52,7 +52,16 @@ func getVersionResp(data []byte) []byte {
 	}
 	_, ok := versionJSON[dloadName]
 	if ok && ret["new_version"] != versionString && versionString >= selfUpdateMinVersion {
-		ret["can_autoupdate"] = true
+		canUpdate := true
+		if runtime.GOOS != "windows" {
+			// On UNIX, if we're running under a regular user,
+			//  but with CAP_NET_BIND_SERVICE set on a binary file,
+			//  and we're listening on ports <1024,
+			//  we won't be able to restart after we replace the binary file,
+			//  because we'll lose CAP_NET_BIND_SERVICE capability.
+			canUpdate, _ = util.HaveAdminRights()
+		}
+		ret["can_autoupdate"] = canUpdate
 	}
 
 	d, _ := json.Marshal(ret)
