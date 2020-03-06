29d24726667b9ccf060d670298bfc7c4a19981bf
Simon Zolin <s.zolin@adguard.com>
2020-03-06 13:50:07 +0300

    minor

--- util/tls.go
+++ util/tls.go
@@ -4,12 +4,21 @@ import (
 	"crypto/x509"
 	"io/ioutil"
 	"os"
+	"runtime"
 
 	"github.com/AdguardTeam/golibs/log"
 )
 
 // LoadSystemRootCAs - load root CAs from the system
+// Return the x509 certificate pool object
+// Return nil if nothing has been found.
+//  This means that Go.crypto will use its default algorithm to find system root CA list.
+// https://github.com/AdguardTeam/AdGuardHome/issues/1311
 func LoadSystemRootCAs() *x509.CertPool {
+	if runtime.GOOS != "linux" {
+		return nil
+	}
+
 	// Directories with the system root certificates, that aren't supported by Go.crypto
 	dirs := []string{
 		"/opt/etc/ssl/certs", // Entware
